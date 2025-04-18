64f9a59457ba85aeb870a4a14afb01bcf4d5822a
lars <larsvkr@outlook.com>
2025-04-18 10:31:12 +0200

    player object build and drawn on the screen. Constant made for player and implemented player in game loop

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pygame
 from constants import *
+import player
+
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    player_1 = player.Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         
@@ -13,9 +17,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        
+        player_1.draw(screen)
 
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
+
+
 
 if __name__ == "__main__":
     main()
